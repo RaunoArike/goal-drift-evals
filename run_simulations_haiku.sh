9d6ee6ea3360d6648ea5844596db4220ad37b933
Elizabeth Donoway <elizabeth.donoway@gmail.com>
2025-01-30 18:49:08 -0800

    Update run_simulations_haiku.sh

--- run_simulations_haiku.sh
+++ run_simulations_haiku.sh
@@ -8,7 +8,7 @@ STEPS_ARRAY=(32 16 8 4 2)
 # Run simulations with increasing steps
 for steps in "${STEPS_ARRAY[@]}"; do
     echo "Running simulation with ${steps} steps..."
-    uv python run_simulation.py \
+    uv run python run_simulation.py \
         --source "${SOURCE}" \
         --target "${TARGET}" \
         --num_steps "${NUM_STEPS}" \
